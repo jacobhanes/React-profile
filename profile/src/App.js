f5a7bc02d2894545c217aaff32a27677cfe107c6
jake hanes <jhanesx@yahoo.com>
2019-11-14 14:20:46 -0700

    started new profile page/ made landing page

---
+++ profile/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import Landing from "./components/landing/index"
+
+function App() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ profile/src/components/landing/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css"
+
+const Landing = () => {
+    return (
+        <div className="wrapper">
+            <button className="land-btn">
+                <h2>
+                    hello
+                </h2>
+            </button>
+        </div>
+    )
+}
+
+export default Landing
